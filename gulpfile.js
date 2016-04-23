6a82a217521e520cd5c4dd7022591a1ba9108087
Jishaal Kalyan <jishaal@gmail.com>
2016-04-23 20:44:30 +1200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass({
+            includePaths: require('node-neat').includePaths
+        }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(livereload());;
+});
+
+gulp.task('sass:watch', function () {
+    livereload.listen();
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch']);
